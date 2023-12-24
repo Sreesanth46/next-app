dac6b05a35e374baa1f3c0a164ad3ccf4fe4c072
Sreesanth <sreesanth46@gmail.com>
2023-12-24 16:42:38 +0530

    feat: client side form validation

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,34 +1,48 @@
 'use client';
 
-import React from 'react';
-import { Button, TextArea, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
+import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type TIssueForm = z.infer<typeof createIssueSchema>
 
 export default function NewIssuePage() {
-  const { register, handleSubmit } = useForm<IssueForm>();
-  const router = useRouter()
+  const router = useRouter();
+  const [error, setError] = useState('');
+  const { register, handleSubmit, formState: { errors } } = useForm<TIssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
 
-  const submit = async (data: IssueForm) => {
-    await axios.post('/api/issues', data)
-    router.push('/issues')
-  } 
+  const submit = async (data: TIssueForm) => {
+    try {
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setError('An error occurred')
+    }
+  };
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(submit)}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register('title')} />
-      </TextField.Root>
-      <TextArea placeholder="Description" {...register('description')} />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className='mb-5' color='red'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className='space-y-3' onSubmit={handleSubmit(submit)}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register('title')} />
+        </TextField.Root>
+        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+        <TextArea placeholder="Description" {...register('description')} />
+        {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 }
