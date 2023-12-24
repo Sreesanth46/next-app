226ee7e7fa7212a0f67145e78cee19b3fad8d6b4
Sreesanth <sreesanth46@gmail.com>
2023-12-24 16:49:01 +0530

    feat: extract error message component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/components/ErrorMessage';
 
 type TIssueForm = z.infer<typeof createIssueSchema>
 
@@ -38,9 +39,9 @@ export default function NewIssuePage() {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
-        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <TextArea placeholder="Description" {...register('description')} />
-        {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
