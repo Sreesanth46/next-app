dd2638dfa7950e45b485fb1e9f9de6f807022192
Sreesanth <sreesanth46@gmail.com>
2023-12-31 01:41:05 +0530

    refactor: error callout component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,52 +1,55 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes';
-import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import axios from 'axios';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Spinner from '@/components/Spinner';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
+import ErrorCallout from '@/components/error/ErrorCallout';
+import ErrorMessage from '@/components/error/ErrorMessage';
 import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
-import ErrorMessage from '@/components/ErrorMessage';
-import Spinner from '@/components/Spinner';
+import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes';
 
-type TIssueForm = z.infer<typeof createIssueSchema>
+type TIssueForm = z.infer<typeof createIssueSchema>;
 
 export default function NewIssuePage() {
   const router = useRouter();
   const [error, setError] = useState('');
   const [submitting, setSubmitting] = useState(false);
-  const { register, handleSubmit, formState: { errors } } = useForm<TIssueForm>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<TIssueForm>({
     resolver: zodResolver(createIssueSchema)
   });
 
   const onSubmit = handleSubmit(async (data: TIssueForm) => {
     try {
-      setSubmitting(true)
+      setSubmitting(true);
       await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
-      setSubmitting(false)
-      setError('An error occurred')
+      setSubmitting(false);
+      setError('An error occurred');
     }
   });
 
   return (
     <div className="max-w-xl">
-      {error && (
-        <Callout.Root className='mb-5' color='red'>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form className='space-y-3' onSubmit={onSubmit}>
+      <ErrorCallout>error</ErrorCallout>
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <TextArea placeholder="Description" {...register('description')} />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={submitting}>Submit New Issue {submitting && <Spinner />}</Button>
+        <Button disabled={submitting}>
+          Submit New Issue {submitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
